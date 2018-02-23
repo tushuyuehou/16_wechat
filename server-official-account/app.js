757a8b527f0cc95b31e96525ff68b8b80cf7d07a
tushuyuehou <751588440@qq.com>
2018-02-23 15:56:36 +0800

    wechat

--- server-official-account/app.js
+++ server-official-account/app.js
@@ -1,5 +1,19 @@
 const EXPRESS = require('express');
+const WECHAT = require('wechat');
+
+let config = {
+    appid:'wx80c3985aa6fbb1bc',
+    token:'weixin',
+    encodingAESKey:'cH68U9vhbWmvQn8TmedYeyI63MjAc83yFc5PmPF2iEW'
+};
 
 let app = new EXPRESS();
+
+app.use(EXPRESS.query());
+
+app.get('/',WECHAT(config,(req,res,next)=>{
+    let message = req.weixin;
+    console.log(message);
+}));
 
 app.listen(3000);
